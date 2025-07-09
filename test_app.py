3467669a706467a58b7633f3277fbb8e5e92a5b5
Bocha12 <danielboc88@gmail.com>
2025-07-09 14:45:18 -0600

    changing to flask

--- test_app.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import saludar
-
-class TestSaludar(unittest.TestCase):
-    def test_saludo(self):
-        self.assertEqual(saludar("Daniel"), "Hola, Daniel!")
-
-if __name__ == '__main__':
-    unittest.main()
